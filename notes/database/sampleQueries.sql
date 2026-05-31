5f299e3cf168efed7f9e8cb96e7a2c4308ef7a19
Marc Duby <mduby@broadinstitute.org>
2026-05-31 15:50:49 -0400

    db: added sample queries

---
+++ notes/database/sampleQueries.sql
@@ -0,0 +1,16 @@
+
+
+
+.headers on
+.mode column
+
+
+select * from gene_set where gene_set_id > 60 limit 20;
+
+
+select provenance_node_id, dcc_url from provenance_node where gene_set_id = 167;
+
+select * from gene_set_gene_symbol where gene_set_id = 167;
+
+
+
